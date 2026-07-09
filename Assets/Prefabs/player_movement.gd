24d2b4f7dc14d18fd3ece79d13930ce6880ea725
Fran Albiol <franalbiolf@gmail.com>
2026-07-09 10:11:23 +0200

    fixed camera

--- Assets/Prefabs/player_movement.gd
+++ Assets/Prefabs/player_movement.gd
@@ -7,31 +7,35 @@ const JUMP_VELOCITY = 4.5
 
 @onready var camera_3d: Camera3D = %Camera3D
 
-func _ready():
+func _ready() -> void:
 	camera_3d.current = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not is_multiplayer_authority():
 		return
 	
 	if event is InputEventMouseMotion:
-		camera_3d.rotate_y(-event.relative.x * sensitivity)    
+		rotate_y(-event.relative.x * sensitivity)    
 		camera_3d.rotate_x(-event.relative.y * sensitivity)
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-90), deg_to_rad(90))
-  
+
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Jump
-	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (camera_3d.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -40,4 +44,3 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
